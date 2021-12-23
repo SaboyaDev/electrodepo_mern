3069c0b066748573584e3de496fb6bafd9e26b6b
SaboyaDev <saboyadev@gmail.com>
2021-12-23 02:57:03 -0500

    Added the products from products.js as components to the homescreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -8,7 +9,7 @@ function App() {
 			<Header />
 			<main className='py-3'>
 				<Container className='text-center'>
-					<h1>Welcome to Electro-Depot</h1>
+					<HomeScreen />
 				</Container>
 			</main>
 			<Footer />
