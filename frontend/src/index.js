6756628b969629a441f05a9d56f81a41475b4916
SaboyaDev <saboyadev@gmail.com>
2022-01-27 01:28:12 -0500

    Created the Redux Store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,20 @@
-import { render } from 'react-dom';
-import App from './App';
-import './bootstrap.min.css';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import store from './store'
+import App from './App'
+import './bootstrap.min.css'
+import './index.css'
+import reportWebVitals from './reportWebVitals'
 
-const rootElement = document.getElementById('root');
-render(<App />, rootElement);
+const rootElement = document.getElementById('root')
+render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	rootElement
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
